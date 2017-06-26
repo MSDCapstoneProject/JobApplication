eed855de5434ad6438c05644a6179a0cf4de1096
Neville <nevil_dabre@yahoo.com>
2017-06-25 22:53:37 -0400

    sequelize case sensitive

--- models/index.js
+++ models/index.js
@@ -2,7 +2,7 @@
 
 var fs = require('fs');
 var path = require('path');
-var Sequelize = require('Sequelize');
+var Sequelize = require('sequelize');
 var env = process.env.NODE_ENV || "development";
 var config = require(path.join(__dirname, '..', 'config', 'config.json'))[env];
 var sequelize = new Sequelize(config.database, config.username, config.password, config);
