6c6f4b11fab832939d4063490beafe5e21f9b8ab
Geuntae Kim <Gkim6898@conestogac.on.ca>
2017-06-26 07:49:26 -0400

    Adding header related to access

--- server.js
+++ server.js
@@ -23,6 +23,17 @@ app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
 
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
+app.disable('etag');
+
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(bodyParser.urlencoded({ extended: true })); // parse application/x-www-form-urlencoded
